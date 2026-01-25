c2737c1ee08ca9b353c7422a846d802846d666b5
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-01-25 17:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (56.92%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -5,8 +5,8 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
         if(k==1) return 0;
-        for(int i=0; i+k-1<n; i++){
-            miniDiff =min(miniDiff, nums[i+k-1] -nums[i]);
+        for(int i=k-1; i<n; i++){
+            miniDiff =min(miniDiff, nums[i] -nums[i-k+1]);
 
         }
 
