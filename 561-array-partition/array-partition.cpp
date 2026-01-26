0081b4b2926bb82428a9dab90749ec4b460e6387
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-01-26 09:36:03 +0530

    Time: 10 ms (56.06%) | Memory: 32.1 MB (19.81%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -3,14 +3,9 @@ public:
     int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int sum = 0;
-        for(int i=0; i< nums.size()-1; i+=2){
+        for(int i=0; i< nums.size(); i+=2){
             sum = sum +(nums[i]);
-
-
         }
-
-
         return sum;
-        
     }
 };
