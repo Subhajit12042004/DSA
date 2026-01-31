29e9bbdc6daf39b57f2fd9492affba3e9b5cbf0b
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-01-31 18:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (68.25%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
-        
+        int long long i =1;
+         int ct =0;
+         while((i*i)<= n){
+            i++;
+            ct++;
+         } 
+          return ct;       
     }
 };
