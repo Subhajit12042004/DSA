3f94fbb34d6028531efc16c0617ed0e7006c96a2
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-02-05 22:18:56 +0530

    Time: 13 ms (5.85%) | Memory: 25.9 MB (8.08%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>result;
+        for(int i=0; i<n; i++){
+            result.push_back(nums[(((i+nums[i])%n)+n)%n]);
+        }
+         return result;
+
+        
+    }
+};
