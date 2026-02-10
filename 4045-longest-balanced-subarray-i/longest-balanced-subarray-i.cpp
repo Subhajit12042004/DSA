848a7d6ac4e65dfca6919598d78c874ee6dafb1c
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-02-10 11:45:10 +0530

    Time: 2051 ms (29.98%) | Memory: 530.5 MB (12.58%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int maxlen = 0;
+
+        for(int i=0; i<n; i++){
+            unordered_set<int>oddSet, evenSet;
+            for(int j=i; j<n; j++){
+                if(nums[j] %2 == 0){
+                    evenSet.insert(nums[j]);
+
+                }else{
+                    oddSet.insert(nums[j]);
+                }
+
+                if(oddSet.size() == evenSet.size()){
+                    maxlen = max(maxlen, j-i+1);
+
+                }
+            }
+        }
+
+        return maxlen;
+        
+    }
+};
