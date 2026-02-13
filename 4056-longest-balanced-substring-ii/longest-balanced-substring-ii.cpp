46f412335dc6981dbe8b312d08e5db0d48b2065f
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-02-14 01:38:40 +0530

    Time: 1669 ms (58.23%) | Memory: 246.7 MB (57.86%) - LeetSync

---
+++ 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.size();
+        int ans = 0;
+
+        // 1️⃣ Single-character runs
+        int cnt = 1;
+        for(int i = 1; i < n; i++){
+            if(s[i] == s[i-1]) cnt++;
+            else {
+                ans = max(ans, cnt);
+                cnt = 1;
+            }
+        }
+        ans = max(ans, cnt);
+
+        // 2️⃣ Two-character balanced cases
+        vector<pair<char,char>> pairs = {
+            {'a','b'}, {'a','c'}, {'b','c'}
+        };
+
+        for(auto [x,y] : pairs){
+            unordered_map<int,int> mp;
+            int diff = 0;
+            mp[0] = -1;
+
+            for(int i = 0; i < n; i++){
+                if(s[i] == x) diff++;
+                else if(s[i] == y) diff--;
+                else {
+                    diff = 0;
+                    mp.clear();
+                    mp[0] = i;
+                    continue;
+                }
+
+                if(mp.count(diff))
+                    ans = max(ans, i - mp[diff]);
+                else
+                    mp[diff] = i;
+            }
+        }
+
+        // 3️⃣ Three-character balanced case
+        unordered_map<long long,int> mp;
+        mp[0] = -1;
+
+        int a = 0, b = 0, c = 0;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == 'a') a++;
+            else if(s[i] == 'b') b++;
+            else c++;
+
+            int diffAB = a - b;
+            int diffAC = a - c;
+
+            long long key = ((long long)diffAB << 32) ^ diffAC;
+
+            if(mp.count(key))
+                ans = max(ans, i - mp[key]);
+            else
+                mp[key] = i;
+        }
+
+        return ans;
+    }
+};
