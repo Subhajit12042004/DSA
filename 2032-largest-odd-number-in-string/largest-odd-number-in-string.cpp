6a8e205b0be7b1db28038831547d09118e4f08ca
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-03-11 07:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.67%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,7 +4,7 @@ public:
     int n = num.length();
     for(int i=n-1; i>=0; i--){
         char ch = num[i];
-        if(ch%2!=0){
+        if((ch -'0')%2==1){
             return num.substr(0, i+1);
    
         }
