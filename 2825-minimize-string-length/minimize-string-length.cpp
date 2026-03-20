b71a2419623d293857e9873f47db0f2113ac0bb3
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-03-20 19:58:20 +0530

    Time: 22 ms (30.83%) | Memory: 16.9 MB (43.93%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        unordered_set<char>st;
+        for( char ch :s){
+            st.insert(ch);
+
+        }
+
+        return st.size();
+        
+    }
+};
