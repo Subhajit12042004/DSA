b31cc6dde1cab76d48575606d624f443761b3635
Subhajit Hazra <146538583+Subhajit12042004@users.noreply.github.com>
2026-04-20 00:41:20 +0530

    Time: 34 ms (5.09%) | Memory: 235.4 MB (5.06%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+          int n = nums.size();
+        int premax = nums[0];
+        vector<int>maxarr;
+        vector<int>minarr;
+     
+        for(int i=0; i<n; i++){
+            premax=max(premax,nums[i]);
+            maxarr.push_back(premax);
+
+        }
+        int postmin =INT_MAX;
+        for(int i=n-1; i>=0; i--){
+            postmin = min(postmin,nums[i]);
+            minarr.push_back(postmin);
+        }
+        reverse(minarr.begin(),minarr.end());
+
+        for(int i=0; i<n; i++){
+
+            int diff = maxarr[i]-minarr[i];
+            if(diff<=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
